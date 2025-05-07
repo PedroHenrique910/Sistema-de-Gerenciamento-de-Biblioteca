a7544a66c3c446cc889a26fb4a9a3450e09715c6
PedroHenrique910 <pedro.henriquesilvaribeiro2006@gmail.com>
2025-05-06 21:11:01 -0300

    ...

--- obj/Debug/net9.0/Sistema_de_gerenciamento_de_biblioteca.AssemblyInfo.cs
+++ obj/Debug/net9.0/Sistema_de_gerenciamento_de_biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema_de_gerenciamento_de_biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acda686f844ca92714f394a7db99fcbe89b8265a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dfafa5bd1c841aa3dd948258bbabcc4df234cb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema_de_gerenciamento_de_biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistema_de_gerenciamento_de_biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
